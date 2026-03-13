092c806936d6e0393f2d0d90c2db3bbb14b1e1f5
Awruhabena <lilybampoe63@gmail.com>
2026-03-13 20:01:15 +0000

    Feature3_Word Frequency Count_function_added

--- analyzer.py
+++ analyzer.py
@@ -32,3 +32,15 @@ def clean_text(raw_text):
         cleaned_text = cleaned_text.replace(punctuation, "") # Remove each punctuation character from the text
     cleaned_text = cleaned_text.split() # Split the cleaned text into a list of words
     return cleaned_text
+
+
+
+#FEATURE 3 +
+def count_frequences(cleaned_text):
+    freq_dict = {} # Initialize an empty dictionary to store word frequencies
+    for word in cleaned_text:
+        if  word in freq_dict.keys(): # Check if the word is already a key in the frequency dictionary
+            freq_dict[word] += 1 # If it is, increment its count by 1
+        else:
+            freq_dict[word] = 1 # If it is not, add it to the dictionary with a count of 1
+    return freq_dict
